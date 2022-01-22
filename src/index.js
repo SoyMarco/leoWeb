4ea5b728169c55cda34451db81914f7d804adc85
Marco Antonio Salazar Ramirez <68885812+SoyMarco@users.noreply.github.com>
2022-01-21 22:53:14 -0600

    fix bug service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import { render } from "react-dom";
 import App from "./App.jsx";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 render(<App />, document.getElementById("root"));
-
-serviceWorkerRegistration.register();
